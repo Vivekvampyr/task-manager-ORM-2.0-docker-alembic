8bc20d763ac307149911d3c6f80da13fb82b5b54
Vivekvampyr <rajawatshivam55@gmail.com>
2026-08-27 16:11:43 +0530

    added CORS middleware which allows backend to support different origins

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 from app.db.database import engine
 from app.api import tasks,auth
 
 app = FastAPI(title="Task Manager API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+    allow_credentials=True,allow_methods=["*"],
+    allow_headers=["*"])
+
 app.include_router(tasks.router,prefix="/api")
 app.include_router(auth.router,prefix="/api")
 
